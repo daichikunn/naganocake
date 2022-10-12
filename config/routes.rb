cfe414a4f5fb0d9be1467d8bf9f7b34c5bd3afcd
EC2 Default User <ec2-user@ip-172-31-36-126.ap-northeast-1.compute.internal>
2022-10-12 05:08:23 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,36 @@
 Rails.application.routes.draw do
 
-devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  
+  root to: "public/homes#top"
+  get "public/homes#about"
+  resources :public/items, only: [:index, :show]
+  resources :public/registrations, only: [:new, :create]
+  resources :public/sessions, only: [:new, :create, :destroy]
+  resources :public/customers, only: [:show, :edit, :update]
+  get "public/customers#check"
+  patch "public/customers#withdraw"
+  resources :public/cart_items, only: [:index, :create, :destroy, :destroy_all, :update]
+  resources :public/orders, only: [ :new, :index, :create, :check, :complete, :show]
+ 
+  
+  
+  
+  resources :admin/sessions, only: [:new, :create, :delete]
+  get "admin/homes#top"
+  resources :admin/items, only: [:index, :new, :create, :show, :edit, :update]
+  resources :admin/genres, only: [:index, :create, :edit, :update]
+  resources :admin/customers, only: [:index, :show, :edit, :update]
+  resources :admin/orders, only: [:show, :update]  
+  patch "admin/order_details#update"
 
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
-
-
-root to: "public/homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
