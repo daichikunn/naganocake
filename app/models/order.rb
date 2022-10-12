108940d76ea93c6ae8bdf45f9bb330ebab1d6f6f
EC2 Default User <ec2-user@ip-172-31-36-126.ap-northeast-1.compute.internal>
2022-10-12 21:49:40 +0000

    update

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,9 @@ class Order < ApplicationRecord
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { waiting_for_payment: 0, payment_confirmation: 1, now_at_work: 2, shipping_preparation: 3, sent: 4 }
   
+  
+  belongs_to :customer
+  has_many :order_details
+  
+  
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,4 +2,7 @@ class OrderDetail < ApplicationRecord
   
   enum making_status: { unable_to_start: 0, waiting_for_production: 1, now_at_work: 2, production_complete: 3, sent: 4 }
   
+  belongs_to :order
+  belongs_to :item
+  
 end
