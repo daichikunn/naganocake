06c46b14a3b00f6ecca93ae801a6fe1790640332
EC2 Default User <ec2-user@ip-172-31-36-126.ap-northeast-1.compute.internal>
2022-10-13 13:49:51 +0000

    update

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,11 +1,16 @@
 class Admin::ItemsController < ApplicationController
   def index
+    
+    @items = Item.all
+    
   end
 
   def new
+    @item = Item.new
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
