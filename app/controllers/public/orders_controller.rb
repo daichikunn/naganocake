866504c60a1b95e15a2cbc6764d2ad81f3be9fc4
EC2 Default User <ec2-user@ip-172-31-36-126.ap-northeast-1.compute.internal>
2022-10-24 12:16:45 +0000

    update

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,14 +1,25 @@
 class Public::OrdersController < ApplicationController
 
   def new
-    @order = Order.new(order_params)
+    @order = Order.new
   end
 
   def check
     @order = Order.new(order_params)
-    if @order.invalid?
-      render :new
-    end
+    if params[:order][:address_option] == "0"
+      #paramsはstringだから””で表示
+      # customers.address を表示したい
+      @order.address = current_customer.address
+
+      elsif params[:order][:address_option] == "1"
+        # セレクトボックスを使って、登録した配送先から選んでもらいたい
+        @address = Address.finnd(params[:current_customer_id])
+      else params[:order][:address_option] == "2"
+        # 新しい住所を打ってほしい
+
+      end
+
+
   end
 
   def complete
